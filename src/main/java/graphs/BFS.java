4beb8c14a814e8c40cde9533fb059882a2f4cbf8
unknown <sreetejreddy1998@gmail.com>
2022-02-12 20:39:29 +0530

    feat: Add BFS for Graphs

---
+++ src/main/java/graphs/BFS.java
@@ -0,0 +1,71 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+/*
+V is the number of nodes in our graph
+adjacency list is the graph representation that we follow in this case
+
+Steps -
+1) Iterate over every node to avoid missing unconnected components.
+2) if the node is not visited, perform BFS on it
+3) add the element to your queue, and while the queue is not empty perform step 4
+4) de-queue your element and add its adj nodes to the queue and de-queued element
+   to your bfs, mark it as visited.
+5) return your BFS array
+ */
+public class BFS {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adjList =  new ArrayList<>();
+        adjList.add(new ArrayList<>());
+        adjList.add(new ArrayList<Integer>(){
+            {
+                add(2);
+                add(3);
+            }
+        });
+        adjList.add(new ArrayList<Integer>(){
+            {
+                add(1);
+                add(3);
+            }
+        });
+        adjList.add(new ArrayList<Integer>(){
+            {
+                add(1);
+                add(2);
+            }
+        });
+        System.out.println(BFSForGraph(3, adjList));
+    }
+
+
+    public static ArrayList<Integer> BFSForGraph( int V, ArrayList<ArrayList<Integer>> adjacencyList) {
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean[] visited = new boolean[V + 1];
+
+        for(int i = 1; i <= V; i++ ) {
+            if(!visited[i]) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
+                visited[i] = true;
+
+                while(!queue.isEmpty()) {
+                    Integer start = queue.poll();
+                    bfs.add(start);
+                    for(Integer node: adjacencyList.get(start)) {
+                        if(!visited[i]) {
+                            visited[i] = true;
+                            queue.add(node);
+                        }
+                    }
+                }
+            }
+        }
+        return bfs;
+    }
+}
